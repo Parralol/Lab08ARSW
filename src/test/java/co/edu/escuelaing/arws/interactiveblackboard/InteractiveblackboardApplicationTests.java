146008c320f9aa5e1c6ee1c179338aa0e807f029
Parralol <110953563+Parralol@users.noreply.github.com>
2024-07-09 10:53:32 -0500

    Add files via upload

---
+++ src/test/java/co/edu/escuelaing/arws/interactiveblackboard/InteractiveblackboardApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.arws.interactiveblackboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveblackboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
